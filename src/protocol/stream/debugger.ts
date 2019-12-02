ab63147f637b83bedb93d9b06700c1b8524f124b
Michael Yeates <michaeljyeates@gmail.com>
2019-12-02 19:53:09 +0000

    Add prefix option

--- src/protocol/stream/debugger.ts
+++ src/protocol/stream/debugger.ts
@@ -11,8 +11,11 @@ import {
 } from "../messages";
 
 export class EOSIOStreamConsoleDebugger extends stream.Writable {
+    private prefix: string;
+
     constructor(options: any){
         super({objectMode: true});
+        this.prefix = options.prefix || '';
     }
 
     _write(chunk: any, encoding: string, callback: (error?: (Error | null)) => void): void {
@@ -21,34 +24,34 @@ export class EOSIOStreamConsoleDebugger extends stream.Writable {
 
         switch (chunk[1]){
             case 'handshake_message':
-                console.log(`Received handshake from ${chunk[2].p2p_address}`);
+                console.log(`${this.prefix} handshake from ${chunk[2].p2p_address}`);
                 break;
             case 'signed_block':
                 const sb_msg = <SignedBlockMessage>msg;
                 const block_num_hex = sb_msg.previous.substr(0, 8); // first 64 bits
                 const block_num = parseInt(block_num_hex, 16) + 1;
-                console.log(`Received block #${block_num} signed by ${sb_msg.producer}`);
+                console.log(`${this.prefix} block #${block_num} signed by ${sb_msg.producer}`);
                 break;
             case 'time_message':
                 const t_msg = <TimeMessage>msg;
-                console.log(`Received time message ${t_msg}`);
+                console.log(`${this.prefix} time message ${t_msg}`);
                 break;
             case 'go_away_message':
                 const ga_msg = <GoAwayMessage>msg;
                 let reason = GoAwayMessage.reasons[ga_msg.reason];
-                console.log(`Received go away message ${reason}`);
+                console.log(`${this.prefix} go away message ${reason}`);
                 break;
             case 'notice_message':
                 const n_msg = <NoticeMessage>msg;
-                console.log(`Received notice message, lib : ${n_msg.known_trx.pending}, head ${n_msg.known_blocks.pending}`);
+                console.log(`${this.prefix} notice message, lib : ${n_msg.known_trx.pending}, head ${n_msg.known_blocks.pending}`);
                 break;
             case 'request_message':
                 const r_msg = <RequestMessage>msg;
-                console.log(`Received request message`);
+                console.log(`${this.prefix} request message`);
                 break;
             case 'sync_request_message':
                 const sr_msg = <SyncRequestMessage>msg;
-                console.log(`Received sync request message`);
+                console.log(`${this.prefix} sync request message`);
                 break;
             default:
                 // console.log('Unknown chunk', chunk);
