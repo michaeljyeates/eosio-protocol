09c65f75009601a340d66f7b3166a6cbe6623b88
Michael Yeates <michaeljyeates@gmail.com>
2019-12-04 12:32:08 +0000

    Store client identifier in debugging stream

--- src/protocol/stream/debugger.ts
+++ src/protocol/stream/debugger.ts
@@ -20,6 +20,7 @@ Writable stream which takes the object output from EOSIOStreamDeserializer and l
 
 export class EOSIOStreamConsoleDebugger extends stream.Writable {
     private prefix: string;
+    private client_identifier: string = '';
 
     constructor(options: any){
         super({objectMode: true});
@@ -32,53 +33,60 @@ export class EOSIOStreamConsoleDebugger extends stream.Writable {
 
         const prefix = `${this.prefix}  [${chunk[1]}]`;
 
+        let log_msg: string = '';
+
         switch (chunk[1]){
             case 'handshake_message':
-                console.log(`${prefix} handshake from ${chunk[2].p2p_address}`);
+                log_msg = `handshake from ${chunk[2].p2p_address}`;
+                this.client_identifier = chunk[2].p2p_address;
                 break;
             case 'time_message':
                 const t_msg = <TimeMessage>msg;
-                console.log(`${prefix} time message dst: ${t_msg.dst}, org: ${t_msg.org}, rec: ${t_msg.rec}, xmt: ${t_msg.xmt}`);
+                log_msg = `time message dst: ${t_msg.dst}, org: ${t_msg.org}, rec: ${t_msg.rec}, xmt: ${t_msg.xmt}`;
                 break;
             case 'go_away_message':
                 const ga_msg = <GoAwayMessage>msg;
                 let reason = GoAwayMessage.reasons[ga_msg.reason];
-                console.log(`${prefix} [${chunk[1]}] go away message ${reason}`);
+                log_msg = `go away message ${reason}`;
                 break;
             case 'notice_message':
                 const n_msg = <NoticeMessage>msg;
                 const notice_mode = NoticeMessage.modes[n_msg.known_blocks.mode];
-                // console.log(n_msg);
+                // log_msg = n_msg);
                 if (notice_mode === 'normal'){
-                    console.log(`${prefix} notice message, remote server is ${n_msg.known_blocks.pending} blocks ahead`);
+                    log_msg = `notice message, remote server is ${n_msg.known_blocks.pending} blocks ahead`;
                 }
                 else {
-                    console.log(`${prefix} ${notice_mode} notice message, lib : ${n_msg.known_trx.pending}, head ${n_msg.known_blocks.pending}`);
+                    log_msg = `${notice_mode} notice message, lib : ${n_msg.known_trx.pending}, head ${n_msg.known_blocks.pending}`;
                 }
                 break;
             case 'request_message':
                 const r_msg = <RequestMessage>msg;
-                console.log(`${prefix} request message`);
+                log_msg = `request message`;
                 break;
             case 'sync_request_message':
                 const sr_msg = <SyncRequestMessage>msg;
-                console.log(`${prefix} sync request message`);
+                log_msg = `sync request message`;
                 break;
             case 'signed_block':
                 const sb_msg = <SignedBlockMessage>msg;
                 const block_num_hex = sb_msg.previous.substr(0, 8); // first 64 bits
                 const block_num = parseInt(block_num_hex, 16) + 1;
-                console.log(`${prefix} #${block_num} signed by ${sb_msg.producer}`);
+                log_msg = `#${block_num} signed by ${sb_msg.producer}`;
                 break;
             case 'packed_transaction':
                 const pt_msg = <PackedTransactionMessage>msg;
                 const trx_bin = Serialize.hexToUint8Array(pt_msg.packed_trx);
                 const trx_id = sha256(trx_bin);
-                console.log(`${prefix} Transaction ${trx_id}`);
+                log_msg = `Transaction ${trx_id}`;
                 break;
-            default:
-                console.log('Unknown chunk', chunk);
-                break;
+        }
+
+        if (!log_msg){
+            console.log('Unknown chunk', chunk);
+        }
+        else {
+            console.log(`${prefix} - ${this.client_identifier} ${log_msg}`);
         }
 
         callback();
